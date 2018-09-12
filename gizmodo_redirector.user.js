9dd301b1dcf7001bd3bf9b7adf0aed811f6db2f7
Kenji Iguchi <needle@Twip-II.local>
2018-09-12 11:23:30 +0900

    Update to work with latest site changes

--- gizmodo_redirector.user.js
+++ gizmodo_redirector.user.js
@@ -2,14 +2,17 @@
 // @name            Gizmodo language redirector
 // @namespace       http://www.heistak.com/
 // @description     Redirects Gizmodo.jp pages to the original English pages
-// @match           http://gizmodo.jp/*
-// @match           http://www.gizmodo.jp/*
-// @version         1.0.0
+// @match           *://gizmodo.jp/*
+// @match           *://www.gizmodo.jp/*
+// @version         1.1.0
 // ==/UserScript==
 
-var entry_detail = document.getElementById("entry_detail");
-var anchors = entry_detail.getElementsByTagName("a");
+var entry_detail = document.querySelector("div.p-post-content small");
+if (entry_detail === null) {
+	return;
+}
 
+var anchors = entry_detail.getElementsByTagName("a");
 for (var i = anchors.length - 1; i >= 0; i--) {
 	if (anchors[i].href.indexOf("gizmodo.com") != -1 || anchors[i].href.indexOf("gawker.com") != -1) {
 		var dest = anchors[i].href;
